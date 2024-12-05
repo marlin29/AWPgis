2071895c265964d05637d1db3d4d8f1be74dde97
Marla Gallegos <115099482+marlin29@users.noreply.github.com>
2024-12-05 12:20:50 -0600

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -4,13 +4,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.17.1/firebase-messaging-comp
 
 // Configuración de Firebase (reemplaza con tus datos)
 const firebaseConfig = {
-  apiKey: "AIzaSyCLATOoJaHm1ik6fCWHJYAkPgIhMQ5B5O4",
-  authDomain: "the-chill-spot-b94ae.firebaseapp.com",
-  projectId: "the-chill-spot-b94ae",
-  storageBucket: "the-chill-spot-b94ae.firebasestorage.app",
-  messagingSenderId: "519997067329",
-  appId: "1:519997067329:web:023cffe155db50c797f6c0",
-  measurementId: "G-NS2XWVYFF2"
+  apiKey: "AIzaSyCqF-YNsr11Gv52DzxQyXo4zcyN_Z6UDF4",
+  authDomain: "gisellelopez-9987c.firebaseapp.com",
+  projectId: "gisellelopez-9987c",
+  storageBucket: "gisellelopez-9987c.firebasestorage.app",
+  messagingSenderId: "771274362295",
+  appId: "1:771274362295:web:66fc986e7f94665d876c32",
+  measurementId: "G-6F6H47RNEP"
 };
 
 // Inicializa Firebase
